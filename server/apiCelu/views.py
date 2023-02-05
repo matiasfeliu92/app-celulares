2f9c6b98cba27768cf3cae287374a1bf2e78eff6
matiasfeliu92 <matumazparrote@gmail.com>
2023-02-05 17:32:01 -0300

    feat(back): add celulares_todos and celulares_todos functions to test api

--- server/apiCelu/views.py
+++ server/apiCelu/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import FileResponse, Http404, JsonResponse
+from .scrap import celulares
+from .models import Celulares
 
 # Create your views here.
+@csrf_exempt
+def index(request):
+    return JsonResponse({'mensaje': 'Hola mundo!, bienvenidos a api celu'})
+
+@csrf_exempt
+def celulares_todos(request):
+    celulares = Celulares.objects.all().values()
+    print(celulares)
+    return JsonResponse([dict(row) for row in celulares.values()], safe=False)
+    # return JsonResponse({'mensaje': 'Hola mundo!, bienvenidos a api celu'})
+
+@csrf_exempt
+def nuevo_celular(request):
+    for celu in celulares:
+        print(celu.nombre)
+    return JsonResponse({'mensaje': 'Hola mundo!, bienvenidos a api celu'})

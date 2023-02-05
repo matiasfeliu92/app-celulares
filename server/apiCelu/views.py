8d1bce391d087359ebca62cb8431644ac48fdc26
matiasfeliu92 <matumazparrote@gmail.com>
2023-02-05 19:18:13 -0300

    refactor(back): update views.py, change language to english, add new_brand and update new_cell_phone function

--- server/apiCelu/views.py
+++ server/apiCelu/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import FileResponse, Http404, JsonResponse
 from .scrap import celulares
-from .models import Celulares
+from .models import Cell_phones, Brands
+import json
 
 # Create your views here.
 @csrf_exempt
@@ -11,14 +12,39 @@ def index(request):
     return JsonResponse({'mensaje': 'Hola mundo!, bienvenidos a api celu'})
 
 @csrf_exempt
-def celulares_todos(request):
-    celulares = Celulares.objects.all().values()
-    print(celulares)
-    return JsonResponse([dict(row) for row in celulares.values()], safe=False)
+def all_cell_phones(request):
+    cell_phones = Cell_phones.objects.all().values()
+    print(cell_phones)
+    return JsonResponse([dict(row) for row in cell_phones.values()], safe=False)
     # return JsonResponse({'mensaje': 'Hola mundo!, bienvenidos a api celu'})
 
 @csrf_exempt
-def nuevo_celular(request):
-    for celu in celulares:
-        print(celu.nombre)
-    return JsonResponse({'mensaje': 'Hola mundo!, bienvenidos a api celu'})
+def new_cell_phone(request):
+    if request.method == 'POST':
+        # body_unidecode = request.body.decode('utf-8')
+        cell_phone = json.loads(request.body)
+        print(cell_phone)
+
+        name = cell_phone['name']
+        price = cell_phone['price']
+        description = cell_phone['description']
+        url_web = cell_phone['url_web']
+        url_image = cell_phone['url_image']
+
+        new_cellphone = Cell_phones(name=name, price=price, description=description, url_web=url_web, url_image=url_image)
+        new_cellphone.save()
+        
+    return JsonResponse({'mensaje': 'nuevo celular insertado'})
+
+@csrf_exempt
+def new_brand(request):
+    if request.method == 'POST':
+        brand = json.loads(request.body)
+        print(brand)
+
+        name = brand['name']
+
+        neww_brand = Brands(name=name)
+        neww_brand.save()
+
+    return JsonResponse({'mensaje': 'nueva marca insertada'})

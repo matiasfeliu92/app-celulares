49487b0543b7a86edb5ebd8911eae7586bacc2d6
matiasfeliu92 <matumazparrote@gmail.com>
2023-03-12 17:59:05 -0300

    refactor(back): add AllProducts view into entities

--- server/config/config.ts
+++ server/config/config.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm";
 import dotenv from 'dotenv'
 import { Product } from "../models/products.models";
 import { Category } from "../models/categorias.models";
+import { AllProducts } from "../models/productsView.models";
 dotenv.config()
 
 export const AppDataSource = new DataSource({
@@ -11,7 +12,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    entities: [Product, Category],
+    entities: [Product, Category, AllProducts],
     logging: true,
     synchronize: true
 })
